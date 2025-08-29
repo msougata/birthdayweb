4d0804bc657ba03ec3ff8e1deb3c2ff680029aad
msougata <142104827+msougata@users.noreply.github.com>
2025-08-30 02:06:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,16 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js' // JS for navbar toggles
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // make sure this is imported
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/birthdayweb/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
